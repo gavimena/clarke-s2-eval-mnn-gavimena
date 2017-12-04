6cf18d9571c7fe358ecc38bc849f88715d1e7ab1
Gabi Mena <gabriela.grojas@gmail.com>
2017-12-04 17:04:17 +0100

    trying functions

---
+++ js/index.js
@@ -0,0 +1,22 @@
+//
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+// }
+
+// var clickButton = document.getElementById('try');
+// clickButton.addEventListener('click', function() {
+//   var number = document.getElementById('take-number').value;
+//   alert(number);
+// });
+'use strict';
+function hola() {
+  alert("Hola");
+}
+
+// var buttonNames = document.querySelector('.button');
+// buttonNames.addEventListener('click', function() {
+//   var inputText = document.querySelector('.name').value;
+//   alert('Hola '+ inputText);
+// });
