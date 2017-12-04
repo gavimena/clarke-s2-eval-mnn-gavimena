0af9872b9a9b26036bd11f27d2128b8b3bc66331
Gabi Mena <gabriela.grojas@gmail.com>
2017-12-04 21:38:20 +0100

    comparation done

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-//
-// function getRandomInt(min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-// }
-
-// var clickButton = document.getElementById('try');
-// clickButton.addEventListener('click', function() {
-//   var number = document.getElementById('take-number').value;
-//   alert(number);
-// });
 'use strict';
-function hola() {
-  alert("Hola");
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-// var buttonNames = document.querySelector('.button');
-// buttonNames.addEventListener('click', function() {
-//   var inputText = document.querySelector('.name').value;
-//   alert('Hola '+ inputText);
-// });
+var numToGuess = getRandomInt(1, 101);
+console.log(numToGuess);
+var textSug = document.getElementById('text-suggestion');
+var buttonCompare = document.getElementById('button-try');
+buttonCompare.addEventListener('click', function () {
+  var numTaken = parseInt(document.getElementById('take-number').value);
+  if(numTaken === numToGuess) {
+    textSug.innerHTML = 'Has acertado';
+  } else if (numTaken < numToGuess) {
+    textSug.innerHTML = 'El número es muy bajo';
+  } else {
+    textSug.innerHTML = 'El número es muy alto';
+  }
+});
