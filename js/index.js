083654863787a9e6f805eafaf21f21842a07aaf4
Gabi Mena <gabriela.grojas@gmail.com>
2017-12-07 20:45:18 +0100

    history done and styles for the input none

--- js/index.js
+++ js/index.js
@@ -10,15 +10,21 @@ var numToGuess = getRandomInt(1, 101);
 console.log(numToGuess);
 var textSug = document.getElementById('text-suggestion');
 var buttonCompare = document.getElementById('button-try');
+var buttonSave = document.getElementById('button-save');
 var tries = document.getElementById('tries-number');
+var nameAndSave = document.querySelector('.save');
+var numTaken = document.getElementById('take-number');
+var nameTaken = document.getElementById('take-name');
+
 var count = 0;
 
-buttonCompare.addEventListener('click', function () {
-  var numTaken = parseInt(document.getElementById('take-number').value);
+buttonCompare.addEventListener('click', function() {
+  var numTakenValue = parseInt(numTaken.value);
 
-  if(numTaken === numToGuess) {
+  if(numTakenValue === numToGuess) {
     textSug.innerHTML = 'Has acertado';
-  } else if (numTaken < numToGuess) {
+    nameAndSave.style.display = 'flex';
+  } else if (numTakenValue < numToGuess) {
     textSug.innerHTML = 'El número es muy bajo';
   } else {
     textSug.innerHTML = 'El número es muy alto';
@@ -26,3 +32,26 @@ buttonCompare.addEventListener('click', function () {
   count += 1;
   tries.innerHTML = count;
 });
+
+buttonSave.addEventListener('click', clickSave);
+
+function clickSave(event){
+  saveName();
+  reset();
+}
+
+function saveName() {
+  var nameTakenValue = (nameTaken.value);
+  var listNames = document.getElementById('history-names');
+
+  listNames.innerHTML += '<li>' + nameTakenValue + ' ' + count + ' ' + 'intentos' + '</li>';
+}
+
+function reset() {
+  count = 0;
+  tries.innerHTML = count;
+  numTaken.value = '';
+  nameTaken.value = '';
+  nameAndSave.style.display = 'none';
+  numToGuess = getRandomInt(1, 101);
+}
